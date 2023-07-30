e56b91791065e96c22d2e7b47ab1bd33e4c0ef0a
karine <karinedeveloper@gmail.com>
2023-07-30 18:13:59 -0300

    adiciona função leapYear.ts

--- src/leapYear.ts
+++ src/leapYear.ts
@@ -5,5 +5,13 @@
  * @returns array of leap years between startYear and endYear
  */
 export const leapYear = (startYear: number, endYear: number): number[] => {
-  return [];
+  const leapYears: number[] = [];
+
+  for (let year = startYear; year <= endYear; year++) {
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+      leapYears.push(year);
+    }
+  }
+
+  return leapYears;
 };
